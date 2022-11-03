c6b32ee142bad8135b85b622584c9a1959562937
kwiats <pawuskwiatkowski@gmail.com>
2022-11-03 17:46:32 +0100

    moved all code under src

---
+++ src/libs/dictionary.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+word_category = Table(
+    "word_category",
+    Base.metadata,
+    Column("wordId", ForeignKey("words.id")),
+    Column("categoryId", ForeignKey("category.id")),
+)
+
+
+class Word(Base):
+    __tablename__ = "words"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    word = Column(String)
+    translated_word = Column(String)
+
+    children = relationship(
+        "Category",
+        secondary=word_category,
+        back_populates="parents",
+    )
+
+    def __repr__(self) -> str:
+        return f"<Words(id= {self.id}, word={self.word}, translated_word={self.translated_word}"
+
+
+class Category(Base):
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    category = Column(String)
+
+    parents = relationship(
+        "Word",
+        secondary=word_category,
+        back_populates="children",
+    )
+
+    def __repr__(self) -> str:
+        return f"<Category(id= {self.id}, category={self.category}"
+
+
+engine = create_engine("postgresql://localhost/pawelkwiatkowski", echo=True)
+Base.metadata.create_all(engine)
