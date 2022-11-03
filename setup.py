fa5da4d2fa34f4ab48e584b64bb9d72ef92eff84
kwiats <pawuskwiatkowski@gmail.com>
2022-11-03 19:35:04 +0100

    update functionality

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
+from src.model.base import Base
+
+from sqlalchemy import create_engine
+
 if __name__ == "__main__":
-    print("Hello world!")
+    engine = create_engine("postgresql://localhost/pawelkwiatkowski", echo=True)
+    Base.metadata.create_all(engine)
